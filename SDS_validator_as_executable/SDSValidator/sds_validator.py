00f80693c3572cec7648eca9f1c24c3fd5d19a23
mohankashyap <mohan.tita@gmail.com>
2023-12-18 14:13:25 -0500

    test SDS_as_assembler_library

--- SDS_validator_as_executable/SDSValidator/sds_validator.py
+++ SDS_validator_as_executable/SDSValidator/sds_validator.py
@@ -41,7 +41,7 @@ class SDSValidatorTest():
         """
         #self.dir_rules = os.path.join(os.path.dirname(__file__)) + "/rules/"
         #self.dir_rules = "SDSValidator\\rules/"
-        self.dir_rules = os.path.join(os.getcwd(),"rules")
+        self.dir_rules = os.path.join(os.getcwd(), "rules")
         self.index_associated = index_associated
 
 
